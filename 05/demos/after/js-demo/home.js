8ae894b7264cee3aaef1ad0a6e84a48b448d31f0
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-24 16:13:55 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,14 +1,4 @@
 
-/*let price;
-showMessage(price);
-console.log(price);*/
- /*a=5;
-console.log(a)
-var a;*/
-for(var i=0; i<10; i++)
-{
-    setTimeout(()=>{
-        console.log(i)
-    },
-    1100)
-}
+let price=20.99;
+showMessage(typeof price);
+
