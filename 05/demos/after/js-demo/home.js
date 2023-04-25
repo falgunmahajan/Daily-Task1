c408f4d62da7c964907052aedbc49c8fdd05287a
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-25 16:57:35 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,17 +1,5 @@
 
-function showProductId()
-{
-    let  productId = 12345;
-
-    function fix()
-    {
-        let productId = 46367;
-        console.log("in fix: "+productId);
-    }
-    fix();
-    
-    console.log("in showProductId: "+productId);
-}
-
-showProductId();
+productId=134
+console.log(productId);
+let productId=12;
 
