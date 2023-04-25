a0d43c9897ee0cac5a9f1d4ad2674dd1cac0f6f1
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-25 16:21:23 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,9 +1,70 @@
+var obj1={
+    "Name":"Falgun",
+    "location":{
+        "city":"Jammu",
+        "pincode":180001
+    }
+};
 
-function showProductId(productId)
+var obj2={
+    "Name":"Falgun",
+    "location":{
+        "city":"Jammu",
+        "pincode":180001
+    }
+};
+var obj=[obj1,obj2]
+for(var i of obj)
 {
-    //let  productId = 12345;
-    //console.log(productId);
-}
-console.log(productId);
-showProductId(125324);
+    for(var j in i)
+    {
 
+        if(typeof i[j]== "object")
+        {
+             
+        for(var k in i[j])
+        {
+            if(k=="pincode")
+            console.log(i[j][k])
+        }
+    }
+        else
+        {
+        console.log(i[j])
+       }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// function showProductId(productId)
+// {
+//     let  productId = 12345;
+
+//     function fix()
+//     {
+//         let productId = 46367;
+//         console.log()
+//     }
+    
+    
+//     console.log(productId);
+// }
+
+// showProductId(125324);
+// console.log(productId);
+// let a=[{Name:"falgun"},{lastName:"Mahajan"}]
+// for(var i of a)
+// {
+//     for(var j in i)
+//     {
+//         console.log(i[j]);
+//     }
+// }
