f6df7b3cbe9fabadbf88c422b9fc5beff9d4a274
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-26 18:28:25 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,4 +1,9 @@
 let values=Array.of(1,2,3);
-var val=values.pop();
+var val=values.shift();
 console.log(val)
+
 console.log(values)
+let arr=[4,1,7,3]
+var result=arr.unshift(2)
+console.log(result)
+console.log(arr)
