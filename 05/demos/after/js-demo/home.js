20613daff17d2de191c254a53a27641be08c1ae3
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 11:51:59 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,6 +1,5 @@
-const values=['a','b', 'c','d','e'];
-const newValues=values.slice(1,3);
-console.log(values);
-values.splice(2,2,'f')
-console.log(values);
-console.log(values.indexOf('f'));
+let myFunction = function()
+{
+    console.log('Here is a message')
+}
+myFunction();
