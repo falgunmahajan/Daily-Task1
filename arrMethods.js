f68561ffa3c0f8653a1ad3308709ed0163675564
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 16:28:14 +0530

    initial

--- arrMethods.js
+++ arrMethods.js
@@ -1,53 +1,79 @@
 
-function product(item)
-{
-    return(item%2==0 && item%5==0)
+// function product(item)
+// {
+//     return(item%2==0 && item%5==0)
 
-}
-
-
-let arr=[]
-for(i=1; i<=50; i++)
-{
-    arr.push(i)
-}
-const val = arr.filter(product)
-console.log(val)
-
-let person=["Falgun","Sangeeta","Sapna","Neha","Alka"]
-console.log(person.filter(validName))
-function validName(name)
-{
-    return(name.length>=5)
-}
-let months=['jan', 'feb', 'mar', 'apr', 'may', 'june']
-months.splice(1,4);
-console.log(months)
+// }
 
 
+// let arr=[]
+// for(i=1; i<=50; i++)
+// {
+//     arr.push(i)
+// }
+// const val = arr.filter(product)
+// console.log(val)
 
-let arr1=[1,2,3,4,5,6]
-console.log(arr1.find(item=>item%2==0))
+// let person=["Falgun","Sangeeta","Sapna","Neha","Alka"]
+// console.log(person.filter(validName))
+// function validName(name)
+// {
+//     return(name.length>=5)
+// }
+// let months=['jan', 'feb', 'mar', 'apr', 'may', 'june']
+// months.splice(1,4);
+// console.log(months)
 
 
 
-let ar1=[3,1,8,2,4]
-ar1.sort()
-console.log(ar1)
-
-let arr2=[1,2,3,4,5,6]
-let initial=0;
-const val1=arr2.reduce((accumulator,current)=>accumulator+current,initial)
-console.log(val1)
+// let arr1=[1,2,3,4,5,6]
+// console.log(arr1.find(item=>item%2==0))
 
 
 
-let arr3=[7,2,1,4,3,6]
-const val2=((arr3.map(item=>item)).filter(item=>item%2!=0)).sort()
-console.log(val2)
+// let ar1=[3,1,8,2,4]
+// ar1.sort()
+// console.log(ar1)
+
+// let arr2=[1,2,3,4,5,6]
+// let initial=0;
+// const val1=arr2.reduce((accumulator,current)=>accumulator+current,initial)
+// console.log(val1)
 
 
-let ar=[3,10,8,2,4]
-ar.sort((a,b)=>a-b)
-console.log(ar)
+
+// let arr3=[7,2,1,4,3,6]
+// const val2=((arr3.map(item=>item)).filter(item=>item%2!=0)).sort()
+// console.log(val2)
+
+
+// let ar=[3,10,8,2,4]
+// ar.sort((a,b)=>a-b)
+// console.log(ar)
+
+
+// let arr=[3,6,2,5,8,9,13,1]
+// let val=arr.filter(isPrime)
+// console.log(val);
+
+// function isPrime(item)
+// {
+//     //var count=0;
+//     if(item!=1)
+//     {
+//     for(var num=2; num<=item/2; num++)
+//     {
+       
+//       if(item%num!=0)
+//         {
+//             //count++;
+//             return true;
+//         }
+//     }
+//     // if(count==0)
+//     // {
+//     //     return true;
+//     // }
+// }
+// }
 
