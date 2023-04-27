8bdcbf67e07df0e316fb7c2ba1ce0a79e176f696
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 12:40:12 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,5 +1,24 @@
-let myFunction = function()
+let myFunction = function(message,firstName)
 {
-    console.log('Here is a message')
+    console.log(message)
+    console.log(firstName)
 }
-myFunction();
+myFunction('Hello','Falgun');
+
+let key = 42;
+function getSecretCode(value)
+{
+    let keyGenerator = function()
+    {
+        let key=12;
+        console.log("in keyGenertor:", key);
+        return key
+    }
+    let code = value*keyGenerator();
+    console.log("in getSecretCode:", key);
+    return code;
+}
+let secretCode = getSecretCode(2)
+showMessage(secretCode);
+
+changePercentOff(50)
--- 05/demos/after/js-demo/utils.js
+++ 05/demos/after/js-demo/utils.js
@@ -1,3 +1,8 @@
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
+
+function changePercentOff(percentage)
+{
+    document.getElementById("percent-off").textContent=percentage+'% OFF';
+}
