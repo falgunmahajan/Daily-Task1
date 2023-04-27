1d784574f5cd6e909ccb2652f80e516cc47a56dd
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 13:41:46 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -4,15 +4,12 @@ let person = {
     name:'John',
     age:32,
     partTime:false,
-    showInfo:function(name)
-    {
-        showMessage(name);
-    }
 };
-// person.name="Falgun";
-// person['age']=22
-// showMessage(person.name+" "+person['age']);
-person.showInfo(24);
-changePercentOff(typeof person.showInfo)
+function incrementAge(person)  
+{
+    person.age++;
+}
+incrementAge(person);
+showMessage(person.age);
 
 
