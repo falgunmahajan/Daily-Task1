f4e805872f221ee586a6da3abce5d6b51b67fc75
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 11:31:23 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,9 +1,5 @@
-let values=Array.of(1,2,3);
-var val=values.shift();
-console.log(val)
-
-console.log(values)
-let arr=[4,1,7,3]
-var result=arr.unshift(2)
-console.log(result)
-console.log(arr)
+const values=['a','b', 'c','d','e'];
+const newValues=values.slice(1,3);
+console.log(values);
+values.splice(2,2,'f')
+console.log(values);
