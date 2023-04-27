7a12b17015c84b7cdd88664c6c1cf489b6f2a94a
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-27 17:07:28 +0530

    initial

--- 05/demos/after/js-demo/home.js
+++ 05/demos/after/js-demo/home.js
@@ -1,7 +1,2 @@
-let now = new Date();
-showMessage(now.toDateString());
-
-//changePercentOff(Math.abs(-42));
-changePercentOff(Math.random());
-let s = 'hello'
-showMessage(s.charAt(1))
+const header = document.getElementById("message");
+header.style.fontWeight='900';
