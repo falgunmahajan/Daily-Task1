94976579e82c50c84ae9c53d04241e8da084fcf9
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-28 15:57:49 +0530

    initial

---
+++ opt/demo.js
@@ -0,0 +1,19 @@
+'use strict'; 
+(function() {
+
+  function registerUser(firstName,lastName,age)
+  {
+    let person={
+      firstName,
+      lastName,
+      age,
+      isAdult()
+      {
+        return this.age>=18
+      }
+    };
+    display(person.isAdult())
+    display(Object.keys(person));
+  }
+  registerUser('Falgun', 'Mahajan',25)
+})();
