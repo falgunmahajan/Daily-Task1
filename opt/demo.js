a8bed4d17fa908d2cb78d48a18f4fc38b9a44970
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-29 13:06:55 +0530

    initial

--- opt/demo.js
+++ opt/demo.js
@@ -1,16 +1,13 @@
 'use strict'; 
 (function() {
 
-    let person1={
+    let person={
       firstName : 'Falgun',
       lastName : 'Mahajan',
       age : 24,
     };
-    let person2=Object.create(Object.prototype,{
-      firstName:{value:'Falgun', enumerable:true, writable:true,configurable:true},
-      lastName:{value:'Mahajan', enumerable:true, writable:true,configurable:true},
-      age:{value:24, enumerable:true, writable:true,configurable:true}
-    })
-    display(person1);
-    display(person2);
+   for(var propertyName in person)
+   {
+    display(propertyName+ " : " +person[propertyName])
+   }
 })();
