03749dd899bc468784df26692a4f1521447fa78f
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-29 17:41:56 +0530

    initial

--- opt/demo.js
+++ opt/demo.js
@@ -1,10 +1,15 @@
 'use strict'; 
 (function() {
-  let myFunction=function(){}
-    display(myFunction.prototype);
-  let person = {firstName:'Falgun'};
-  display(person.prototype);
-  display(person.__proto__);
-
-
+  function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+Person.prototype.age = 29;
+  display(Person.prototype);
+  let falgun = new Person('Falgun', 'Mahajan');
+  let falak = new Person('Falak', 'Mahajan')
+  falak.__proto__.age=30;
+  display(falgun.__proto__);
+  display(falak.__proto__);
+  display(Person.prototype===falgun.__proto__);
 })();
