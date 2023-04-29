9c8b1e986607c042ed00646685adb44d5f82ca3d
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-29 16:47:31 +0530

    initial

--- opt/demo.js
+++ opt/demo.js
@@ -1,27 +1,10 @@
 'use strict'; 
 (function() {
+  let myFunction=function(){}
+    display(myFunction.prototype);
+  let person = {firstName:'Falgun'};
+  display(person.prototype);
+  display(person.__proto__);
 
-    let person={
-      name:
-      {
-        firstName : 'Falgun',
-        lastName : 'Mahajan'
-      },
-      age : 24,
-    };
-    Object.defineProperty(person, 'fullName', {
-      get : function(){
-        return this.name.firstName+ "  " +this.name.lastName;
-      },
-      set: function(value){
-        var nameParts= value.split(' ');
-        this.name.firstName = nameParts[0];
-        this.name.lastName = nameParts[1];
-      }
-    });
-    person.fullName="Manas Mahajan"
-    display(person.fullName);
-    display(person.name.firstName);
-    display(person.name.lastName);
 
 })();
