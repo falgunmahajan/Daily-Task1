bc14a8f5b01d51cb0ead0e375005f67e116e1ec8
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-29 15:45:55 +0530

    initial

--- opt/demo.js
+++ opt/demo.js
@@ -6,12 +6,6 @@
       lastName : 'Mahajan',
       age : 24,
     };
-    Object.defineProperty(person,'firstName', {enumerable:false})
-    for(let propertyName in person)
-    {
-      display(propertyName+ ':' +person[propertyName])
-    }
-    display(Object.keys(person));
-    display(JSON.stringify(person));
-    display(person.firstName);
+    Object.defineProperty(person,'firstName', {configurable:false})
+    Object.defineProperty(person,'firstName', {writable:true})
 })();
