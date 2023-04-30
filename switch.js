053d380bcfc2328632a2a74d8d0a72affff9f754
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-30 20:33:19 +0530

    initial

--- switch.js
+++ switch.js
@@ -15,3 +15,45 @@ function simpleSwitch()
         
     }
 }
+function multipleCase()
+{
+    let color="Red";
+    switch(color)
+    {
+        case "Red":
+        case "Pink":
+            console.log("The color is red");
+            break;
+        case "Blue":
+        case "Light Blue":
+        case "Dark Blue":
+            console.log("The color is blue");
+        case "Gray":
+        case "Grey":
+            console.log("The color is grey");
+            break;
+        default:
+            console.log("Unknown color");
+            break;
+    }
+}
+function forgetABreak()
+{
+    let productId = 2;
+    switch(productId)
+    {
+        case 1:
+            console.log("HL Road Frame=Black, 58");
+            break;
+        case 2:
+            console.log("Sport-100 Helmet, Red");
+        case 3:
+            console.log("Mountain Bike Socks, M");
+            break;
+        default:
+                console.log("Unknown Product");
+                break;
+        
+    }
+
+}
