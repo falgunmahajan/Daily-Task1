0607f837dd558b785eb546168a0857add4b7fe74
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-30 20:05:29 +0530

    initial

---
+++ switch.js
@@ -0,0 +1,17 @@
+function simpleSwitch()
+{
+    let productId = 3;
+    switch(productId)
+    {
+        default:
+            console.log("Unknown Product");
+            break;
+        case 1:
+            console.log("Product 1");
+            break;
+        case 2:
+            console.log("Product 2");
+            break;
+        
+    }
+}
