d7c3d7d2457d0d69cf4d500815f2eafbc9811462
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-30 22:39:01 +0530

    initial

---
+++ label.js
@@ -0,0 +1,13 @@
+function labelSample()
+{
+    even:
+    for(let index=1; index<=10; index++)
+    {
+        if(index%2==1)
+        {
+            continue even;
+        }
+        console.log(index);
+    }
+}
+labelSample();
