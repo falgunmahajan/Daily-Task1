3b86c1edbba321b54d03c7337af570c1277147c6
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-30 22:57:37 +0530

    initial

---
+++ mathSample.js
@@ -0,0 +1,46 @@
+'use strict';
+
+(function()
+{
+    let price = 200; 
+    let result = 0;
+    console.log("price = " + price.toString());
+
+    //Addition
+    result = price+100;
+    console.log("price + 100 = " + result.toString());
+
+    //Subtraction
+    result = price-10;
+    console.log("price - 10 = " + result.toString());
+
+    //Multiplication
+    result = price*2;
+    console.log("price * 2 = " + result.toString());
+
+    //Division
+    result = price/2;
+    console.log("price / 2 = " + result.toString());
+
+    //Exponentiation
+    result = price**2;
+    console.log("price ** 2 = " + result.toString());
+
+    // Modulus
+    result = price%3;
+    console.log("price %3  = " + result.toString());
+
+    //Increment
+    result = price++;
+    console.log("result = price++ = " + result.toString());
+    console.log("price = " + price.toString());
+    result = ++price;
+    console.log("result = ++price = " + result.toString());
+
+    //Decrement
+    result = price--;
+    console.log("result = price-- = " + result.toString());
+    console.log("price = " + price.toString());
+    result = --price;
+    console.log("result = --price = " + result.toString());
+})();
