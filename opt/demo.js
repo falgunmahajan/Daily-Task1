a0b98287fa2c2de5c294370a62644c2185819768
falgunmahajan <falgunmahajan1999@gmail.com>
2023-04-30 13:29:39 +0530

    initial

--- opt/demo.js
+++ opt/demo.js
@@ -5,11 +5,13 @@
     this.lastName = lastName;
   }
 Person.prototype.age = 29;
-  let falgun = new Person('Falgun', 'Mahajan');
+  let manas = new Person('Manas', 'Mahajan');
   let falak = new Person('Falak', 'Mahajan')
-  falgun.age=24
-  display(falgun.age);
-  display(falgun.__proto__.age);
-  display(falgun.hasOwnProperty('age'));
+  Person.prototype={age:18};
+  display(Person.prototype);
+  let mayuri = new Person('Mayuri', 'Mahajan')
+  display(manas.age);
+  display(falak.age);
+  display(mayuri.age);
   
 })();
